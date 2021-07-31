bf481a8b0450707d8e0685778280978251a0120c
Abhishek Bhure <abhiab7411@gmail.com>
2021-07-31 15:49:52 +0530

    Existing code added

---
+++ DHANVANTARI/js/index.js
@@ -0,0 +1,36 @@
+const myslide = document.querySelectorAll('.myslide');
+let counter = 1;
+slidefun(counter);
+
+let timer = setInterval(autoSlide, 8000);
+function autoSlide() {
+	counter += 1;
+	slidefun(counter);
+}
+function plusSlides(n) {
+	counter += n;
+	slidefun(counter);
+	resetTimer();
+}
+
+function resetTimer() {
+	clearInterval(timer);
+	timer = setInterval(autoSlide, 8000);
+}
+
+function slidefun(n){
+	
+	let i;
+	for(i = 0;i<myslide.length;i++){
+		myslide[i].style.display = "none";
+	}
+	
+	if(n > myslide.length){
+	   counter = 1;
+	   }
+	if(n < 1){
+	   counter = myslide.length;
+	   }
+	myslide[counter - 1].style.display = "block";
+
+}
